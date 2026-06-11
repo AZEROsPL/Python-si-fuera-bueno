6a06a06d01692d1adfcc09db0a175a811563f104
AZEROsPL <andrei20070910@gmail.com>
2026-06-10 19:23:22 -0500

    Delete Decisión directory

--- Decisión/6 intervalo.py
+++
@@ -1,7 +0,0 @@
-a = float(input("Ingrese el valor minimo:"))
-b = float(input("Ingrese el valor maximo:"))
-c = float(input("Ingrese un número averiguar si se encuentra en el intervalo [{},{}]: ".format(a, b)))
-if c >= a and c <= b:
-    print("El número insertado se encuentra en el intervalo [{},{}]: ".format(a, b))
-else:
-    print("El número insertado no se encuentra en el intervalo [{},{}]: ".format(a, b))
